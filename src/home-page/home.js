f4e3d8a2a01ce4a0f1155f50680f5471b40f3cf8
Eric Joo <ericjaeminjoo@gmail.com>
2018-11-06 15:49:46 -0500

    Add club description section along with add Hover.css and Bulma utilities

--- src/home-page/home.js
+++ src/home-page/home.js
@@ -1,2 +1,12 @@
 require("normalize.css/normalize.css");
 require("../styles/home.scss");
+
+// $(document).ready(function () {
+//     $(window).scroll(function () {
+//         $("#arrow-down").css("opacity", 1 - $(window).scrollTop() / 250);
+//     });
+// });
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("DOMContentLoaded", "page-index")
+});
