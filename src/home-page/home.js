1242c567215b9a31ddfb6cfdbf00d4a2b693be67
Eric Joo <ericjaeminjoo@gmail.com>
2018-11-07 16:52:44 -0500

    Add navbar and footer partials

--- src/home-page/home.js
+++ src/home-page/home.js
@@ -4,7 +4,6 @@ require("../styles/spacing.scss");
 require("../../node_modules/counterup/jquery.counterup.min.js");
 require("../../node_modules/waypoints/lib/jquery.waypoints.min.js");
 
-
 $(document).ready(function () {
     $(window).scroll(function () {
         $("#arrow-down").css("opacity", 1 - $(window).scrollTop() / 650);
