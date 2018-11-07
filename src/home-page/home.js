0078ce7e5e10283cfaca072a5cbd03adfc3a8b1b
Eric Joo <ericjaeminjoo@gmail.com>
2018-11-07 14:50:42 -0500

    Add social media section and custom spacing sass file

--- src/home-page/home.js
+++ src/home-page/home.js
@@ -1,8 +1,22 @@
 require("normalize.css/normalize.css");
 require("../styles/home.scss");
+require("../styles/spacing.scss");
+require("../../node_modules/counterup/jquery.counterup.min.js");
+require("../../node_modules/waypoints/lib/jquery.waypoints.min.js");
 
-// $(document).ready(function () {
-//     $(window).scroll(function () {
-//         $("#arrow-down").css("opacity", 1 - $(window).scrollTop() / 250);
-//     });
-// });
+
+$(document).ready(function () {
+    $(window).scroll(function () {
+        $("#arrow-down").css("opacity", 1 - $(window).scrollTop() / 650);
+    });
+});
+
+/*=====================================================
+				   Counter
+======================================================*/
+$(function () {
+    $('.counter').counterUp({
+        delay: 10,
+        time: 7500
+    });
+});
