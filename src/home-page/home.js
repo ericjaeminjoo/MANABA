3dec830a4c341405cd00f109d9df08f5c6d32634
Eric Joo <ericjaeminjoo@gmail.com>
2018-11-08 16:56:00 -0500

    Add smooth-scroll javascript function for anchor tags

--- src/home-page/home.js
+++ src/home-page/home.js
@@ -22,3 +22,14 @@ $(function () {
         time: 7500
     });
 });
+
+// Smooth scrolling
+$(function () {
+    $("a.smooth-scroll").click(function (event) {
+        event.preventDefault();
+        var section = $(this).attr("href");
+        $('html, body').animate({
+            scrollTop: $(section).offset().top
+        }, 1200, "easeInOutExpo");
+    });
+});
