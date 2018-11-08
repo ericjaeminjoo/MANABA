05a3abca7e83ec1c2da587b343002e20960ea67f
Eric Joo <ericjaeminjoo@gmail.com>
2018-11-08 17:42:08 -0500

    Add Frequently Asked Questions (FAQ) section

--- src/home-page/home.js
+++ src/home-page/home.js
@@ -3,19 +3,22 @@ require("../styles/home.scss");
 require("../styles/spacing.scss");
 require("../../node_modules/counterup/jquery.counterup.min.js");
 require("../../node_modules/waypoints/lib/jquery.waypoints.min.js");
+var bulmaAccordion = require("../../node_modules/bulma-accordion/dist/js/bulma-accordion.min.js");
 
 $(document).ready(function () {
     // Initiate wowjs for animate on scroll
     new WOW().init();
 
+    // Arrow down decrease opacity as user scrolls down
     $(window).scroll(function () {
         $("#arrow-down").css("opacity", 1 - $(window).scrollTop() / 650);
     });
+
+    // accordions now contains an array of all Accordion instances
+    var accordions = bulmaAccordion.attach();
 });
 
-/*=====================================================
-				   Counter
-======================================================*/
+// Counter function for number values
 $(function () {
     $('.counter').counterUp({
         delay: 10,
