e5caacfc71c8427d1f9013eac5f7899537b8bf8c
Eric Joo <ericjaeminjoo@gmail.com>
2018-11-16 20:46:15 -0500

    Finish image optimization for server image loading time

--- src/team-page/team.js
+++ src/team-page/team.js
@@ -1,3 +1,35 @@
 require("normalize.css/normalize.css");
 require("../styles/team.scss");
 require("../styles/spacing.scss");
+
+$(window).on('load', function () {
+    // Arrow down decrease opacity as user scrolls down
+    $(window).scroll(function () {
+        if ($(this).scrollTop() < 65) {
+            $("#back-to-top").fadeOut();
+        } else {
+            $("#back-to-top").fadeIn();
+        }
+    });
+});
+
+$(function () {
+    $('.description-content').hide();
+    $('.card-content').hover(function () {
+        $(this).children('.description-content').stop().slideDown();
+    }, function () {
+        // slides children up
+        $(this).children('.description-content').stop().slideUp();
+    })
+});
+
+// Smooth scrolling
+$(function () {
+    $("a.smooth-scroll").click(function (event) {
+        event.preventDefault();
+        var section = $(this).attr("href");
+        $('html, body').animate({
+            scrollTop: $(section).offset().top
+        }, 1200, "easeInOutExpo");
+    });
+});
