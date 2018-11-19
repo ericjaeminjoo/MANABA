a28820c753a0142deac344ebb0cfe84d420ffc91
Eric Joo <ericjaeminjoo@gmail.com>
2018-11-19 12:41:03 -0500

    Add events section with styling

--- src/events-page/events.js
+++ src/events-page/events.js
@@ -1,3 +1,14 @@
 require("normalize.css/normalize.css");
 require("../styles/events.scss");
 require("../styles/spacing.scss");
+
+$(document).ready(function () {
+    $("#showModal").click(function () {
+        console.log("AHHHHHHHHH");
+        $(".modal").addClass("is-active");
+    });
+
+    $(".modal-close").click(function () {
+        $(".modal").removeClass("is-active");
+    });
+});
